a8288764e8397d2e35d9597ac7e1e4926fe261af
VictorMR <viketore18@hotmail.com>
2016-11-04 09:30:42 +0100

    parte3

--- AddressApp/src/ch/makery/address/view/PersonOverviewController.java
+++ AddressApp/src/ch/makery/address/view/PersonOverviewController.java
@@ -113,7 +113,45 @@ public class PersonOverviewController {
         	alert.setHeaderText("No person to delete");     
         	alert.setContentText("Please select a row");     
         	alert.showAndWait();
-        }}
+        }
+        
+    }
+    /**
+     * Called when the user clicks the new button. Opens a dialog to edit
+     * details for a new person.
+     */
+    @FXML
+    private void handleNewPerson() {
+        Person tempPerson = new Person();
+        boolean okClicked = mainApp.showPersonEditDialog(tempPerson);
+        if (okClicked) {
+            mainApp.getPersonData().add(tempPerson);
+        }
+    }
+
+    /**
+     * Called when the user clicks the edit button. Opens a dialog to edit
+     * details for the selected person.
+     */
+    @FXML
+    private void handleEditPerson() {
+        Person selectedPerson = personTable.getSelectionModel().getSelectedItem();
+        if (selectedPerson != null) {
+            boolean okClicked = mainApp.showPersonEditDialog(selectedPerson);
+            if (okClicked) {
+                showPersonDetails(selectedPerson);
+            }
+
+        } else {
+            // Nothing selected.
+            
+            Alert alert = new Alert(AlertType.WARNING);     
+        	alert.setTitle("No Selection");     
+        	alert.setHeaderText("No Person Selected");     
+        	alert.setContentText("Please select a person in the table.");     
+        	alert.showAndWait();
+        }
+    }
     
     
 }
